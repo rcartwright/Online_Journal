b65fb1048b50457b699d3384568ea1f3b23a25bd
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2015-04-08 00:49:54 -0700

    add table column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406232855) do
+ActiveRecord::Schema.define(version: 20150407011415) do
 
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150406232855) do
     t.string   "remember_token"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "blog_name"
   end
 
 end
