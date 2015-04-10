a4d388b383d4bb1fab1afb2024b602bd9bf1e149
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2015-04-10 12:41:48 -0700

    remove before filters, change show action for now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_filter :signed_in_user
-  before_filter :correct_user,   only: :destroy
+
 
   # GET /posts
   # GET /posts.json
@@ -15,10 +14,8 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
-
-
-    @blog = Blog.find(params[:id])
-    @post = @blog.posts.find(params[:id])
+    @post = Post.find(params[:id])
+    @blog = @post.blog
     @blog.user = @user
   end
 
