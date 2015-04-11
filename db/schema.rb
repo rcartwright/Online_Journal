7487713a267c8ef6e2a899844a6e141b6038311c
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2015-04-11 01:26:30 -0700

    add friendly_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410204200) do
+ActiveRecord::Schema.define(version: 20150411054305) do
 
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "blog_name"
+    t.string   "slug"
   end
+
+  add_index "blogs", ["slug"], name: "index_blogs_on_slug", unique: true
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
