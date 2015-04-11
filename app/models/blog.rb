cf3819eb098e144cc26a8e974c8e94bb66a71ff2
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2015-04-11 01:28:28 -0700

    extend friendly_id

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,9 @@
 class Blog < ActiveRecord::Base
+
+	extend FriendlyId
+  	friendly_id :blog_name, use: [:slugged, :finders]
+
 	has_many :posts, :dependent => :destroy
 	has_one :user, :dependent => :destroy
+
 end
