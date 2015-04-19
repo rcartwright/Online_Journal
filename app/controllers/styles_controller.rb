4117df709df4c3bede7139a57691550e71474c1a
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2015-04-18 17:51:25 -0700

    remove unnecessary views/actions & fix edit style form

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,48 +1,25 @@
 class StylesController < ApplicationController
-  before_action :set_style, only: [:show, :edit, :update, :destroy]
-
-  # GET /styles
-  # GET /styles.json
-  def index
-    @styles = Style.all
-  end
+  before_action :set_style, only: [:show, :edit, :update]
 
   # GET /styles/1
   # GET /styles/1.json
   def show
   end
 
-  # GET /styles/new
-  def new
-    @style = Style.new
-  end
 
   # GET /styles/1/edit
   def edit
+    @blog = Blog.find(params[:blog_id])
   end
 
-  # POST /styles
-  # POST /styles.json
-  def create
-    @style = Style.new(style_params)
-
-    respond_to do |format|
-      if @style.save
-        format.html { redirect_to @style, notice: 'Style was successfully created.' }
-        format.json { render :show, status: :created, location: @style }
-      else
-        format.html { render :new }
-        format.json { render json: @style.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # PATCH/PUT /styles/1
   # PATCH/PUT /styles/1.json
   def update
+    @blog = Blog.find(params[:blog_id])
     respond_to do |format|
       if @style.update(style_params)
-        format.html { redirect_to @style, notice: 'Style was successfully updated.' }
+        format.html { redirect_to blog_path(@blog), notice: 'Style was successfully updated.' }
         format.json { render :show, status: :ok, location: @style }
       else
         format.html { render :edit }
@@ -51,15 +28,6 @@ class StylesController < ApplicationController
     end
   end
 
-  # DELETE /styles/1
-  # DELETE /styles/1.json
-  def destroy
-    @style.destroy
-    respond_to do |format|
-      format.html { redirect_to styles_url, notice: 'Style was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/views/styles/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@styles) do |style|
-  json.extract! style, :id, :layout, :header_image, :color_theme
-  json.url style_url(style, format: :json)
-end
