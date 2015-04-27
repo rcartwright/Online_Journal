14f89cfe5656fb1f3a558145c595761ff3cdb989
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2015-04-27 15:09:37 -0700

    move unnecessary controller method to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,16 @@
 module ApplicationHelper
-def parent_layout(layout)
-  @view_flow.set(:layout,output_buffer)
-  self.output_buffer = render(file: "layouts/#{layout}")
-end
+
+	def parent_layout(layout)
+  	@view_flow.set(:layout,output_buffer)
+  	self.output_buffer = render(file: "layouts/#{layout}")
+	end
+
+	def layout
+  	if defined?(@style.layout)
+      @style.layout
+		else
+			'admin'
+		end
+	end
+
 end
