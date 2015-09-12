9c937bd7e748e9bcbf862b11c7460672c509e50f
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2015-09-12 16:20:40 -0700

    order posts by newest

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,7 @@ class Blog < ActiveRecord::Base
 	extend FriendlyId
   	friendly_id :blog_name, use: [:slugged, :finders]
 
-	has_many :posts, :dependent => :destroy
+	has_many :posts,-> { order "created_at DESC" }, dependent: :destroy
 	has_one :user, :dependent => :destroy
 	has_one :style, :dependent => :destroy
 	accepts_nested_attributes_for :user
