ba33cdfc0255e249b8f8cd4dfc3177eb13c8a932
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2015-09-20 23:53:53 -0700

    see if fixes heroku

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,8 @@
 module SessionsHelper
   def sign_in(user)
-    cookies.permanent[:remember_token] = user.remember_token
-    self.current_user = user
+    #cookies.permanent[:remember_token] = user.remember_token
+    #self.current_user = user
+    session[:user_id] = user.id
   end
   def signed_in?
     !current_user.nil?
@@ -10,7 +11,8 @@ module SessionsHelper
     @current_user = user
   end
   def current_user
-    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+    #@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+    @current_user = @current_user || User.find_by(id: session[:user_id])
   end
   def current_user?(user)
     user == current_user
