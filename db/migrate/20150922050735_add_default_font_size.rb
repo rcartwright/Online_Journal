530a7be592676d74c7db1594a09269bc545fc6bf
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2015-09-30 22:13:27 -0700

    remove for heroku

--- db/migrate/20150922050735_add_default_font_size.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultFontSize < ActiveRecord::Migration
-  def change
-  	change_column :styles, :font_size, :character, :default => "14px"
-  end
-end
--- db/migrate/20150923051253_change_datatype.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatype < ActiveRecord::Migration
-  def change
-  	change_column :styles, :font_size, :character, :default => "14px"
-  end
-end
