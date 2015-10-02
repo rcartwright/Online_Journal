c7028a0200ec5fad0245c04728a82d9ba0d9fcab
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2015-10-01 19:55:07 -0700

    fix sign in and sign out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,9 @@ module SessionsHelper
   def sign_in(user)
     #cookies.permanent[:remember_token] = user.remember_token
     #self.current_user = user
-    session[:user_id] = user.id
+    #session[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+    self.current_user = user
   end
   def signed_in?
     !current_user.nil?
@@ -11,11 +13,12 @@ module SessionsHelper
     @current_user = user
   end
   def current_user
-        if cookies[:remember_token].present?
+      if cookies[:remember_token].present?
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
     #@current_user ||= User.find_by_remember_token(cookies[:remember_token])
     #@current_user = @current_user || User.find_by(id: session[:user_id])
   end
+end
 
   def current_user?(user)
     user == current_user
