1de50763b060723597fb1c81a919aff91b34faaf
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2015-10-01 23:26:45 -0700

    add new helpers, and change column name, etc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924060247) do
+ActiveRecord::Schema.define(version: 20151002050025) do
 
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 20150924060247) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.string   "remember_token"
+    t.string   "remember_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "blog_id"
